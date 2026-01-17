7a0161ea7b93f991ce1f4122e24aad3f9f26f099
Tino <tinonativa6@gmail.com>
2026-01-17 13:26:18 +0300

    I test Annotated typing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
 from typing import Annotated
 
@@ -22,8 +22,8 @@ async def create_item(item: Item):
 
 @app.get("/library/{item_id}")
 async def library(
-    item_id : str,
-    q : Annotated[str | None, Query(alias="add-q")] = None
+    item_id : Annotated[str, Path(title="the id for search")],
+    q : Annotated[str | None, Query(alias="add-q", title="A var for request")] = None
 ):  
     item = {"item_id" : item_id}
     if q:
