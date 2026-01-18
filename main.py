6639c63a4ca3f41e279f33657ddf09a7b89d73f2
Tino <tinonativa6@gmail.com>
2026-01-18 12:52:28 +0300

    created model for entry data and search data

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 from fastapi import FastAPI, Query, Path
-from pydantic import BaseModel
-from typing import Annotated
+from pydantic import BaseModel, Field
+from typing import Annotated, Literal
+from datetime import datetime
+
+class MusicEntry(BaseModel):
+    title : str = Field(description="a title for the search", min_length=1, max_length=50)
+    artist: str = Field(description="the artist of the music", min_length=1, max_length=50)
+    duration : int = Field(gt=0)
+    genre : Literal["Rock", "Pop", "Jazz", "Techno"]
+    created_at : datetime = Field(default_factory=datetime.now, description="date of creation")
+
+class MusicSearch(BaseModel):
+    filter : Literal["Rock", "Pop", "Jazz", "Techno"] | None = None
+    trie : Literal["title", "created_at"] | None = None
+    limit : int = Field(5, gt=0, le=10, description="limit of result")
+    offset : int = Field(0, ge=0, description="the offset where to search")
+    direction : Literal["asc", "desc"] = "desc"
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-liste = ["toto", "tata"]
-
-
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to the FastAPI application!"}
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
-@app.get("/library/{item_id}")
-async def library(
-    item_id : Annotated[str, Path(title="the id for search")],
-    q : Annotated[str | None, Query(alias="add-q", title="A var for request")] = None
-):  
-    item = {"item_id" : item_id}
-    if q:
-        item.update({"q": q})
-    return item
-    
+@app.get("/search/")
+async def search(params: Annotated[MusicSearch, Query()]):
+    return params    
