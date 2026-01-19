5e5997d66eaa5d6cbec09384e7936f65fd40f99e
Deraniaina Tino Nativa Randrianarisoa <derandri@c1r10s12.42antananarivo.mg>
2026-01-19 10:42:41 +0300

    create search_music_es pour la recherche chez ES avec Query DSL format

--- main.py
+++ main.py
@@ -11,14 +11,58 @@ class MusicEntry(BaseModel):
     created_at : datetime = Field(default_factory=datetime.now, description="date of creation")
 
 class MusicSearch(BaseModel):
+    title : str = Field(description="the search query", min_length=1, max_length=50)
     filter : Literal["Rock", "Pop", "Jazz", "Techno"] | None = None
-    trie : Literal["title", "created_at"] | None = None
+    sorted_by : Literal["title", "created_at", "duration"] | None = None
     limit : int = Field(5, gt=0, le=10, description="limit of result")
     offset : int = Field(0, ge=0, description="the offset where to search")
     direction : Literal["asc", "desc"] = "desc"
+    duration : int = Field(0, ge=0, description="minimum duration in seconds")
 
 app = FastAPI()
 
+async def search_music_es(params: MusicSearch):
+    query = {
+        "query": {
+            "bool": {
+            "must": [],
+            "filter": []
+            },
+        },
+        "from": params.offset,
+        "size": params.limit,
+        "sort": []
+    }
+    if params.title:
+            query["query"]["bool"]["must"].append({
+                "match": {
+                    "title": params.title
+                }
+            })
+    if params.filter:
+            query["query"]["bool"]["filter"].append({
+                "term": {
+                    "genre": params.filter
+                }
+            })
+    if params.duration > 0:
+            query["query"]["bool"]["filter"].append({
+                "range": {
+                    "duration": {
+                        "gte": params.duration
+                    }
+                }
+            })
+    if params.sorted_by:
+            query["sort"].append({
+                params.sorted_by: {
+                    "order": params.direction
+                }
+            })
+    return query # In a real implementation, this would execute the query against Elasticsearch
+    
+
 @app.get("/search/")
 async def search(params: Annotated[MusicSearch, Query()]):
-    return params    
+    results = await search_music_es(params)
+    return results
