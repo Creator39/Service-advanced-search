7dc3279e9f1bdac9c1abb06cbeb416a4c7778dc0
Tino <tinonativa6@gmail.com>
2026-01-19 19:21:51 +0300

    try to learn rabitmq protocol throught practice

--- producer.py
+++ producer.py
@@ -16,27 +16,19 @@ def send_message():
     # Les identifiants sont ceux de ton docker-compose (admin/admin)
     credentials = pika.PlainCredentials('admin', 'admin')
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost', port=5671, credentials=credentials)
+        pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials)
     )
+    queu_name = "music_data"
     channel = connection.channel()
-
-    # 2. Déclaration de la file (La fameuse "Boîte aux lettres")
-    queue_name = 'music_indexing_queue'
-    channel.queue_declare(queue=queue_name, durable=True) 
-    # durable=True : la file survit si RabbitMQ redémarre
-
-    # 3. Envoi du message
-    channel.basic_publish(
-        exchange='',
-        routing_key=queue_name,
-        body=json.dumps(fake_music), # On transforme le dictionnaire en texte JSON
-        properties=pika.BasicProperties(
-            delivery_mode=2,  # Rend le message persistant (il survit au reboot)
-        )
-    )
-    
-    print(f" [x] Envoyé : '{fake_music['title']}' dans RabbitMQ")
+    channel.queue_declare(queue=queu_name)
+    channel.basic_publish(exchange='',
+                          routing_key=queu_name,
+                          body=json.dumps(fake_music)
+                          )
     connection.close()
+    print(f"send {fake_music}")
+
+    
 
 if __name__ == "__main__":
     send_message()
